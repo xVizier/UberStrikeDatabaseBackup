5f03ea5b9f73fc23bbf1cc535295f3593bfbd1bb
xVizier <143770098+xVizier@users.noreply.github.com>
2023-10-04 22:03:15 -0400

    Update

--- DatabaseDump.py
+++ DatabaseDump.py
@@ -16,15 +16,10 @@ os.makedirs(FolderName)
 # Creates the Backup Folder with date as unique identifier
 os.chdir(FolderName)
 # Changes the Directory to the current folder.
-<<<<<<< HEAD
+
 MongoDump = 'mongodump --host localhost:27017 --username uber --password admin --authenticationDatabase admin'
 try:
     os.system(MongoDump)
-=======
-
-try:
-    os.system('cmd /k "mongodump --host localhost:27017 --username uber --password admin --authenticationDatabase admin"')
->>>>>>> 72d9e27aeb6df0accdf45ef41525900a9c4ae581
 except:
     print('Could not execute MONGODUMP command')
 # Opens CMD and Runs the MongoDump Command.
