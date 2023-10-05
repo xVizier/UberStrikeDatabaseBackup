659d6c9a6a1f73bc564273676d1d1a51342c9922
xVizier <GerardWebbJr@westfrontpointllc.com>
2023-10-05 01:46:31 +0000

    Routine Backup

---
+++ DatabaseUpload.py
@@ -0,0 +1,34 @@
+import os
+from github import Github
+
+CurrentDirectory = os.getcwd()
+
+GitAdd = 'git add .'
+GitCommit = 'git commit -m "Routine Backup"'
+GitBranch = 'git branch -M main'
+GitPush = 'git push -u origin main'
+GitPull = 'git pull'
+
+try:
+    os.system(GitPull)
+except:
+    print("Couldn't Execute Command.")
+try:
+    os.system(GitAdd)
+except:
+    print("Couldn't Execute Command.")
+
+try:
+    os.system(GitCommit)
+except:
+    print("Couldn't Execute Command.")
+
+try:
+    os.system(GitBranch)
+except:
+    print("Couldn't Execute Command.")
+
+try:
+    os.system(GitPush)
+except:
+    print("Couldn't Execute Command.")
