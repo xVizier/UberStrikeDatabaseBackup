019ade0a434c5948f95408f6fc974f205a095712
xVizier <143770098+xVizier@users.noreply.github.com>
2023-10-04 21:19:39 -0400

    Routine Backup

--- DatabaseBackup2.py
+++ DatabaseBackup2.py
@@ -12,39 +12,25 @@ FolderName = "Backup_"+FormattedDate
 # Assigns the Folder Name to be created
 CurrentDirectory = os.getcwd()
 # Retrieves the Current Directory from System
-#os.makedirs(FolderName)
+os.makedirs(FolderName)
 # Creates the Backup Folder with date as unique identifier
-#os.chdir(FolderName)
+os.chdir(FolderName)
 # Changes the Directory to the current folder.
-
-#try:
-#    os.system('cmd /k "mongodump --host localhost:27017 --username uber --password admin --authenticationDatabase admin"')
-#except:
-#    print('Could not execute MONGODUMP command')
+print(CurrentDirectory)
+try:
+    os.system('cmd /k "mongodump --host localhost:27017 --username uber --password admin --authenticationDatabase admin"')
+except:
+    print('Could not execute MONGODUMP command')
 # Opens CMD and Runs the MongoDump Command.
 
-#Token = "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"
-
-#Git = Github(Token)
-
-#for repo in Git.get_user().get_repos():
-#    print("Repo:", repo.name)
-
-#repo = Git.get_repo("xVizier/UberStrikeDatabaseBackups")
-#try:
-#    os.system('cmd k/ "cd C:\Program Files\Git"')
-#except:
-#    print("Couldn't Execute Command.")
-
-#try:
-#    os.system('cmd k/ "git-bash.exe"')
-#except:
-#    print("Couldn't Execute Command.")
 GitAdd = 'git add .'
 GitCommit = 'git commit -m "Routine Backup"'
 GitBranch = 'git branch -M main'
 GitPush = 'git push -u origin main'
 GitPull = 'git pull'
+
+print(CurrentDirectory)
+
 try:
     os.system(GitPull)
 except:
